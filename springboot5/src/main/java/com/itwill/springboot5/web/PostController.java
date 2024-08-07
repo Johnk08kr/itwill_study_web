305e2e92044d90ca76e49c5cb8999fc195862aa2
johnk <rnjsdygks12@gmail.com>
2024-08-07 09:48:23 +0900

    SpringBoot Security

--- springboot5/src/main/java/com/itwill/springboot5/web/PostController.java
+++ springboot5/src/main/java/com/itwill/springboot5/web/PostController.java
@@ -1,80 +1,34 @@
 package com.itwill.springboot5.web;
 
+import com.itwill.springboot5.domain.Post;
+import com.itwill.springboot5.dto.*;
+import com.itwill.springboot5.service.PostService;
 import org.springframework.data.domain.Page;
-<<<<<<< HEAD
 import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-=======
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
->>>>>>> dfdd8a5220b4c2bb23c0b91944d326868c9b5b5c
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.itwill.springboot5.domain.Comment;
-import com.itwill.springboot5.dto.CommentRegisterDto;
-import com.itwill.springboot5.dto.CommentUpdateDto;
-import com.itwill.springboot5.service.CommentService;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/api/comment")
-public class CommentController {
-	private final CommentService commentService;
+@Controller
+@RequestMapping("/post")
+public class PostController {
+	private final PostService postService;
 
-	@PreAuthorize("hasRole('USER')")
-	@PostMapping
-	public ResponseEntity<Comment> registerComment(@RequestBody CommentRegisterDto dto) {
-		log.info("registerComment(dto={})", dto);
+	@GetMapping("/list")
+	public void list(@RequestParam(name = "p", defaultValue = "0") int pageNo, Model model) {
+		log.info("list(pageNo={})", pageNo);
 
-		// 서비스 계층의 메소드 호출(댓글 등록 서비스 실행)
-		Comment entity = commentService.create(dto);
-		log.info("Success Save: {}", entity);
+		// 서비스 계층의 메서드를 호출 -> 뷰에 포스트 목록 전달
+		Page<PostListItemDto> list = postService.read(pageNo, Sort.by("id").descending());
+		model.addAttribute("page", list);
 
-		return ResponseEntity.ok(entity);
-	}
-
-	@PreAuthorize("hasRole('USER')")
-	@GetMapping("/all/{postId}")
-	public ResponseEntity<Page<Comment>> getCommentList(@PathVariable Long postId,
-			@RequestParam(name = "p", defaultValue = "0") int pageNo) {
-		log.info("getCommentList(postId={}, pageNo={})", postId, pageNo);
-		Page<Comment> data = commentService.readCommentList(postId, pageNo);
-
-		return ResponseEntity.ok(data);
-	}
-
-	@PreAuthorize("hasRole('USER')")
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Long> deleteComment(@PathVariable Long id) {
-		log.info("deleteComment(id={})", id);
-
-		commentService.delete(id);
-
-		return ResponseEntity.ok(id); // 삭제한 댓글 아이디를 응답으로 보냄.
-	}
-
-	@PreAuthorize("hasRole('USER')")
-	@PatchMapping("/{id}")
-	public ResponseEntity<Long> updateComment(@PathVariable Long id, @RequestBody CommentUpdateDto dto) {
-		log.info("updateComment(id={}, dto={})", id, dto);
-		
-<<<<<<< HEAD
-		// pagination fragment에서 사용하기 위한 정보 현재 요청 주소 정보
+		// pagination fragment에서 사용하기 위한 현재 요청 주소 정보
 		model.addAttribute("baseUrl", "/post/list");
 	}
 
@@ -92,6 +46,7 @@ public class CommentController {
 		postService.create(dto);
 		return "redirect:/post/list";
 	}
+
 	@PreAuthorize("hasRole('USER')")
 	@GetMapping("/details/{id}")
 	public String detail(@PathVariable Long id, Model model) {
@@ -135,14 +90,14 @@ public class CommentController {
 
 	@GetMapping("/search")
 	public String search(PostSearchRequestDto dto, Model model) {
-		log.info("search(dto={}", dto);
+		log.info("search(dto={})", dto);
 
 		Page<PostListItemDto> result = postService.search(dto, Sort.by("id").descending());
 		model.addAttribute("page", result);
-=======
-		commentService.update(dto);
->>>>>>> dfdd8a5220b4c2bb23c0b91944d326868c9b5b5c
-		
-		return ResponseEntity.ok(id); // 업데이트한 댓글의 아이디를 응답으로 보냄.
+
+		// pagination fragment에서 사용할 현재 요청 주소 정보
+		model.addAttribute("baseUrl", "/post/search");
+
+		return "post/list";
 	}
 }
